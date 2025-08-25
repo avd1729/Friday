990a833248a2f817b6190828ba8a113407d92002
Aravind <aravind19112003@gmail.com>
2025-08-25 15:16:51 +0530

    setup helpers

--- src/friday/memory/sqlite_conversation_memory.py
+++ src/friday/memory/sqlite_conversation_memory.py
@@ -1,9 +1,67 @@
 from friday.memory.base_conversation_memory import BaseConversationMemory
-
+import sqlite3
+from datetime import time
 class SqliteConversationMemory(BaseConversationMemory):
-    def __init__(self, session_id = None, user_id = None):
-        super().__init__(session_id, user_id)
+    def __init__(self, session_id = None, user_id = None, db_path="conversation_memory.db"):
+        self.session_id = session_id
+        self.user_id = user_id
+        self.db_path = db_path
 
+        if session_id is None:
+            self.session_id = self._create_new_session(user_id)
+        else:
+            self.session_id = session_id
+
+    def _connect(self):
+        return sqlite3.connect(self)
+    
+    def _init_db(self):
+        conn = self._connect()
+        cursor = conn.cursor()
+
+        # session
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS sessions (
+                session_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id TEXT,
+                created_at REAL
+            )
+            """
+        )
+
+        # messages
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS messages (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                session_id INTEGER,
+                role TEXT,
+                content TEXT,
+                metadata TEXT,
+                timestamp REAL,
+                FOREIGN KEY(session_id) REFERENCES sessions(session_id)
+            )
+            """
+        ) 
+
+        conn.commit()
+        conn.close()
+
+    def _create_new_session(self, user_id):
+        conn = self._connect()
+        cursor = conn.cursor()
+        cursor.execute(
+            """
+            INSERT INTO SESSIONS (user_id, created_at) VALUES (?, ?)
+            """, (user_id, time.time)
+        )
+
+        conn.commit()
+        session_id = cursor.lastrowid
+        conn.close()
+        return session_id
+    
     def add_to_history(self, role, content, metadata = None):
         return super().add_to_history(role, content, metadata)
     
